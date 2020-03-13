3006078072c632cade679b3cc2590b0db716093c
Xiaoooyooo <xiaoooyooo@gmail.com>
2020-03-13 21:36:44 +0800

    some new features

--- city.js
+++ city.js
@@ -1,20 +1,33 @@
 
 let getCityInfoBtn = document.querySelector("[value='Get']")
 let cityCodeInput = document.getElementById('cityCode')
-getCityInfoBtn.addEventListener('click',function(){
+let download = document.getElementById('download')
+getCityInfoBtn.addEventListener('click', function () {
     let cityCode = cityCodeInput.value
-    if(cityCode == '' || /^\d*?$/.test(cityCode) == false){
+    if (cityCode == '' || /^\d*?$/.test(cityCode) == false) {
         alert('仅限数字，空白也不行')
         return
     }
     let url = `https://restapi.amap.com/v3/config/district?keywords=${cityCode}&subdistrict=1&extensions=all&key=44b3613c0d8c4e8bf9344b456b8706c2`
     let xhr = new XMLHttpRequest
-    xhr.open("get",url)
-    xhr.onload = function(){
+    xhr.open("get", url)
+    xhr.onload = function () {
         let res = JSON.parse(this.response).districts[0]
         // console.log(res)
-        console.log("行政区名:",res.name)
+        console.log("行政区名:", res.name)
         console.log(res.polyline)
+        createCSVFile(res.polyline.split(';'), res.name)
     }
     xhr.send()
 })
+function createCSVFile(text, name) {
+    let str = 'X,Y\n'
+    text.forEach(el => {
+        str += el + ',\t\n'
+    })
+    let uri = 'data:text/csv;charset=utf-8,\ufeff' + encodeURIComponent(str);
+    download.href = uri
+    download.download = `${name}.csv`
+    download.style.pointerEvents = 'auto'
+    download.innerText = "This is yours."
+}
